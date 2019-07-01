601942993bf8364e6b54032853903012d1e4b4db
Ricardo Quintanar <lrq1012@gmail.com>
2019-07-01 18:49:21 -0500

    react folders moved

---
+++ build/precache-manifest.9770d7ae1a3acc2768bb7ac43ede6513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68264b5fe43faf12285a7f9e2f8bd78",
+    "url": "/react-fast/index.html"
+  },
+  {
+    "revision": "2547d3a7bf050e616fed",
+    "url": "/react-fast/static/css/main.4626bb82.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-fast/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "2547d3a7bf050e616fed",
+    "url": "/react-fast/static/js/main.bcf21ce3.chunk.js"
+  },
+  {
+    "revision": "97c725e05e06c112fe65",
+    "url": "/react-fast/static/js/runtime~main.31e3cc75.js"
+  }
+]);
