69d4264e3a427c310d4421984e717329ac93260f
Ricardo Quintanar <lrq1012@gmail.com>
2019-07-01 19:11:46 -0500

    Updates

---
+++ precache-manifest.911d3df3198a7cc2f8141fcf6efd4bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc03cd40eea6e961717cf8e58c1ca02",
+    "url": "/react-fast/index.html"
+  },
+  {
+    "revision": "53137d4e0f512c0113c9",
+    "url": "/react-fast/static/css/main.4626bb82.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-fast/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "53137d4e0f512c0113c9",
+    "url": "/react-fast/static/js/main.27d57b02.chunk.js"
+  },
+  {
+    "revision": "97c725e05e06c112fe65",
+    "url": "/react-fast/static/js/runtime~main.31e3cc75.js"
+  }
+]);
